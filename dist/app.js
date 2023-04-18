16ad5a37596204b3c1599172e7fc2806402c27cd
kundishankar <shankar.kundi19@gmail.com>
2023-04-19 00:40:01 +0530

    app folder name changed to src

--- dist/app.js
+++ dist/app.js
@@ -4,6 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3200;
 app.listen(port, () => console.log(`Server started on port : ${port}`));
